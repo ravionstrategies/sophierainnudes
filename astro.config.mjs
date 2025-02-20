7cf7f875612ad46897b6580cdd5327d4c875fbda
ravionstrategies <ravionstrategies@gmail.com>
2025-02-20 20:52:02 +0100

    Fix: Restore performance optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,46 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel/static';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sophierainnudes.com',
   output: 'static',
   adapter: vercel(),
+  compressHTML: true,
   build: {
-    assets: 'assets'
+    inlineStylesheets: 'auto'
   },
   integrations: [
+    compress({
+      CSS: true,
+      HTML: {
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+        collapseWhitespace: true,
+        minifyJS: true,
+        minifyCSS: true
+      },
+      JavaScript: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true
+        }
+      },
+      Image: true,
+      SVG: true
+    }),
     sitemap()
-  ]
+  ],
+  vite: {
+    build: {
+      cssCodeSplit: true,
+      minify: 'terser',
+      cssMinify: true
+    }
+  }
 });
